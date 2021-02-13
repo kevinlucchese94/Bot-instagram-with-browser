ec5ae499fc232be9d5b5613fba7981eac4b79e51
kevinlucchese94 <78358512+kevinlucchese94@users.noreply.github.com>
2021-02-13 11:11:38 +0100

    Update ListHashtag.py

--- ListHashtag.py
+++ ListHashtag.py
@@ -1,3 +1,3 @@
 hashtags = (
-    'travel', 'nature', 'photography', 'travelphotography', 'love', 'photooftheday', 'instagood', 'travelgram', 'picoftheday', 'instagram', 'photo', 'beautiful', 'art', 'like', 'naturephotography', 'follow', 'wanderlust', 'happy', 'adventure', 'instatravel', 'bhfyp', 'fashion', 'travelblogger', 'landscape', 'summer', 'trip', 'style', 'ig', 'explore', 'bhfyp'
-)
+    'inserire', 'cosi', 'glihastag'
+    )
