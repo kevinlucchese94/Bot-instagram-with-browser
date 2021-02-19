f61685f20802e5fdea8a46eed21757e3e1fb98ef
kevinlucchese94 <78358512+kevinlucchese94@users.noreply.github.com>
2021-02-19 09:58:35 +0100

    Update LikeByLocation.py

--- LikeByLocation.py
+++ LikeByLocation.py
@@ -38,7 +38,7 @@ login_link.click()
 sleep(10)
 print(dt_string, "Ho salvato le informazioni")
 
-for n in range(0, 3):
+for n in range(0, 7):
     for hashtag in random.choices(location):  # fa il giro di tutti gli hashtag impostati
         browser.get(hashtag)
         sleep(10)
@@ -48,7 +48,7 @@ for n in range(0, 3):
         like.click()  # click sulla foto
         sleep(10)
 
-        for i in range(0, 10):  # per ogni hashtag metti 10 like
+        for i in range(0, 3):  # per ogni hashtag metti 10 like
 
             like = browser.find_element_by_class_name("fr66n")
             like.click()  # click sul cuore
