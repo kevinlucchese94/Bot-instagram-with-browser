432c79fa500ebebc2345b11f13e583a09beea541
kevinlucchese94 <78358512+kevinlucchese94@users.noreply.github.com>
2021-02-19 09:56:24 +0100

    Update FollowByHashtag.py

--- FollowByHashtag.py
+++ FollowByHashtag.py
@@ -36,9 +36,10 @@ login_link.click()
 sleep(10)
 print(dt_string, "Ho salvato le informazioni")
 
-for n in range(0, 3):
+for n in range(0, 7):
 
     for hashtag in random.choices(hashtags):
+        print("ho scelto l'hashtag", hashtag)
         browser.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
         sleep(10)
 
@@ -47,7 +48,7 @@ for n in range(0, 3):
         follow.click()
         sleep(10)
 
-        for i in range(0, 10):  # per ogni hashtag segui 10 persone
+        for i in range(0, 3):  # per ogni hashtag segui 3 persone
 
             follow = browser.find_element_by_xpath(
                 "/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button")  # click sul follow
