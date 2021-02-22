8488c58c52f43c64aa64db52495f8d9bb12d9835
kevinlucchese94 <78358512+kevinlucchese94@users.noreply.github.com>
2021-02-22 10:10:37 +0100

    Update FollowByHashtag.py

--- FollowByHashtag.py
+++ FollowByHashtag.py
@@ -39,6 +39,7 @@ print(dt_string, "Ho salvato le informazioni")
 for n in range(0, 3):
 
     for hashtag in random.choices(hashtags):
+        print("hashtag", hashtag)
         browser.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
         sleep(10)
 
