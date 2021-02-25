ffa90ac8aa4cb82bc515f4917446288011092475
kevinlucchese94 <78358512+kevinlucchese94@users.noreply.github.com>
2021-02-25 17:19:00 +0100

    Update ListLocation.py

--- ListLocation.py
+++ ListLocation.py
@@ -2,5 +2,11 @@ location = (
     "https://www.instagram.com/explore/locations/562470/arena-di-verona/",
     "https://www.instagram.com/explore/locations/118058804875414/verona/",
     "https://www.instagram.com/explore/locations/512503136/casa-di-giulietta/",
-    "https://www.instagram.com/explore/locations/263844049/san-giovanni-lupatoto/"
+    "https://www.instagram.com/explore/locations/263844049/san-giovanni-lupatoto/",
+    "https://www.instagram.com/explore/locations/219988612/desenzano-del-garda-italy/",
+    "https://www.instagram.com/explore/locations/215190070/sirmione/",
+    "https://www.instagram.com/explore/locations/2088021/gardaland/",
+    "https://www.instagram.com/explore/locations/214693681/valeggio-sul-mincio-italy/",
+    "https://www.instagram.com/explore/locations/110073564077687/borghetto-sul-mincio/",
+    "https://www.instagram.com/explore/locations/249231533/rifugio-malga-lessinia/"
 )
