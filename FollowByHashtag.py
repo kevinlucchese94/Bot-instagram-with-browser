c1c9a661658b10b5a8b14e5f3bce9881d09a34f9
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2023-12-22 12:25:29 +0100

    Update FollowByHashtag.py

--- FollowByHashtag.py
+++ FollowByHashtag.py
@@ -19,45 +19,44 @@ dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 browser.get('https://www.instagram.com/')
 
 # Accetta i cookies
-cookie_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='_g8n0']")))
+cookie_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Accept All')]")))
 cookie_btn.click()
 print(dt_string, "Ho accettato i cookies")
 
 # Inserisci Username e Password
-username_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='username']")))
-password_input = browser.find_element_by_css_selector("input[id='password']")
+username_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
+password_input = browser.find_element_by_css_selector("input[name='password']")
 username_input.send_keys(username)
 password_input.send_keys(password)
 
-login_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+login_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
 login_btn.click()
 print(dt_string, "Ho eseguito l'accesso")
 
 # Salva le informazioni
-save_info_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Salva le informazioni']")))
+save_info_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Salva')]")))
 save_info_btn.click()
 
 for n in range(0, 3):
-    for hashtag in random.choices(hashtags):
-        print("hashtag", hashtag)
-        browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
-        sleep(10)
+  for hashtag in random.choices(hashtags):
+    print("hashtag", hashtag)
+    browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
+    sleep(10)
 
-        # Clicca sulla prima immagine
-        image_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[class='_7UhW9']")))
-        image = image_element.find_element_by_tag_name('img')
-        image.click()
-        sleep(10)
+    # Clicca sulla prima immagine
+    image = wait.until(EC.element_to_be_clickable((By.XPATH, "//img[@alt='Post']")))
+    image.click()
+    sleep(10)
 
-        for i in range(0, 10):
-            like_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='Mi piace']")))
-            like_btn.click()
-            sleep(10)
+    for i in range(0, 10):
+      like_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='like-button u-icon u-icon-heart']")))
+      like_btn.click()
+      sleep(10)
 
-            next_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[class='coreSpriteRightArrow']")))
-            next_btn.click()
-            sleep(10)
+      next_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Avanti')]")))
+      next_btn.click()
+      sleep(10)
 
-            print(dt_string, f"Ho messo {i + 1} Like sull'hashtag {hashtag}")
+      print(dt_string, f"Ho messo {i + 1} Like sull'hashtag {hashtag}")
 
 browser.quit()
