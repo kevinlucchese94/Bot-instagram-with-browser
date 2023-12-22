2e9265d2c04237a13c8005f8cc03fe6d70a793cc
Kevin Lucchese <78358512+kevinlucchese94@users.noreply.github.com>
2023-12-22 12:05:00 +0100

    update test

--- LikeByHashtag.py
+++ LikeByHashtag.py
@@ -1,64 +1,62 @@
 import random
 from datetime import datetime
-from time import sleep
-
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from ListHashtag import *
 from credenziali import *
 
-options = Options()
-options.add_argument('--headless')
+options = webdriver.FirefoxOptions()
+# options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 browser = webdriver.Firefox(options=options)
+wait = WebDriverWait(browser, 10)
+
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 browser.get('https://www.instagram.com/')
 
-login_link = browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]")  # accetta i cookies
-sleep(10)
-login_link.click()
+# Accetta i cookies
+cookie_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/div/div[2]/button[1]")))
+cookie_btn.click()
 print(dt_string, "Ho accettato i cookies")
-print(dt_string, "Inserisco Username e pwd")
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
 
+# Inserisci Username e Password
+username_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
+password_input = browser.find_element_by_css_selector("input[name='password']")
 username_input.send_keys(username)
 password_input.send_keys(password)
 
-login_link = browser.find_element_by_xpath(
-    "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]")  # click su login
-login_link.click()
-sleep(10)
+login_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]")))
+login_btn.click()
 print(dt_string, "Ho eseguito l'accesso")
-login_link = browser.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button")
-login_link.click()
-sleep(10)
-print(dt_string, "Ho salvato le informazioni")
+
+# Salva le informazioni
+save_info_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/section/main/div/div/div/div/button")))
+save_info_btn.click()
 
 for n in range(0, 3):
-    for hashtag in random.choices(hashtags):  # fa il giro di tutti gli hashtag impostati
+    for hashtag in random.choices(hashtags):
         print("hashtag", hashtag)
-        browser.get('https://www.instagram.com/explore/tags/' + hashtag + '/')
+        browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
         sleep(10)
 
-        like = browser.find_element_by_xpath(
-            "/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]/a/div/div[2]")
-        like.click()  # click sulla foto
+        # Clicca sulla prima immagine
+        image = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.v1Nh3")))
+        image.click()
         sleep(10)
 
-        for i in range(0, 10):  # per ogni hashtag metti 10 like
-
-            like = browser.find_element_by_class_name("fr66n")
-            like.click()  # click sul cuore
-            sleep(10)
-            like = browser.find_element_by_xpath("/html/body/div[5]/div[1]/div/div/a[2]")
-            like.click()  # click sulla freccia
+        for i in range(0, 10):
+            like_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fr66n")))
+            like_btn.click()
             sleep(10)
 
-            print(dt_string, "Ho messo", i + 1, "Like sull'hashtag", hashtag)
+            next_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[5]/div[1]/div/div/a[2]")))
+            next_btn.click()
+            sleep(10)
 
+            print(dt_string, f"Ho messo {i + 1} Like sull'hashtag {hashtag}")
 
-browser.close()
+browser.quit()
